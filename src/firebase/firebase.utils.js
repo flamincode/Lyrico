e406b18cba5d2e4e8ed904b538c2d7a17bccbe5f
Alex McCooeye <alex@auvenir.com>
2021-09-07 15:39:27 -0400

    new firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -5,10 +5,11 @@ import 'firebase/auth'
 const config = {
   apiKey: "AIzaSyC9DhbBYpdYtSRiHPqLcTqej1LyDDHKtVs",
   authDomain: "lyrico-db.firebaseapp.com",
+  databaseURL: "https://lyrico-db-default-rtdb.firebaseio.com",
   projectId: "lyrico-db",
   storageBucket: "lyrico-db.appspot.com",
   messagingSenderId: "745969762259",
-  appId: "1:745969762259:web:a6483e679670c04e31b926"
+  appId: "1:745969762259:web:3c8432af998b6d8231b926"
 }
 
 firebase.initializeApp(config)
