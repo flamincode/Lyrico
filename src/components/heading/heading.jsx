f3c8108b673d9d31e6f2d8606965bd81c7c7995a
amccooeye <amccooeye@Macadmins-MacBook-Pro.local>
2021-09-23 14:40:41 -0400

    dropdown

--- src/components/heading/heading.jsx
+++ src/components/heading/heading.jsx
@@ -1,17 +1,27 @@
 import React from 'react'
-import { useHistory } from "react-router-dom";
-import './heading.scss'
+import { useHistory, Link } from "react-router-dom";
 import image from '../../logo.jpg'
+import { Dropdown } from 'semantic-ui-react';
+import './heading.scss'
 
 const Heading = () => {
-  const history = useHistory()
+  
+  const trigger = (
+    <button className={'logo'}><img alt='' src={image}/></button>
+  )
 
-  const handleClick = () => {
-    history.push('/')
-  }
   return (
-    <div className='header'>
-      <button className={'logo'} onClick={handleClick}><img alt='' src={image}/></button>
+    <div className='lyrico-header'>
+      <Dropdown 
+        trigger={trigger}>
+        <Dropdown.Menu>
+          <Link to='/'><Dropdown.Item text='Home'/></Link>
+          <Link to='/teams'><Dropdown.Item text='Teams'/></Link>
+          <Link to='/playerSelection'><Dropdown.Item text='Player Selection'/></Link>
+          <Link to='/rules'><Dropdown.Item text='Rules'/></Link>
+          <Link to='/game'><Dropdown.Item text='Game Play'/></Link>
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
   )
 }
